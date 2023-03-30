845873b80452a469f144ab80119cdd6e2fef1502
Jagoba Inda <jindaardam1@gmail.com>
2023-03-30 04:16:20 +0200

    Pruebas spawneando un cofre y empezado el randomizador de items.

--- src/main/java/es/mariaanasanz/ut7/mods/impl/ExampleMod.java
+++ src/main/java/es/mariaanasanz/ut7/mods/impl/ExampleMod.java
@@ -1,11 +1,33 @@
 package es.mariaanasanz.ut7.mods.impl;
 
+import com.ibm.icu.impl.DayPeriodRules;
+import com.mojang.datafixers.util.Pair;
 import es.mariaanasanz.ut7.mods.base.*;
 import net.minecraft.core.BlockPos;
+import net.minecraft.server.WorldLoader;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.packs.resources.ResourceManager;
+import net.minecraft.util.datafix.fixes.EntitySkeletonSplitFix;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.monster.Skeleton;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.ClipContext;
+import net.minecraft.world.level.DataPackConfig;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.ChestBlock;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.storage.WorldData;
+import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.client.event.MovementInputUpdateEvent;
+import net.minecraftforge.client.event.RenderHighlightEvent;
+import net.minecraftforge.client.model.generators.BlockStateProvider;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.living.LivingDamageEvent;
 import net.minecraftforge.event.entity.living.LivingDeathEvent;
 import net.minecraftforge.event.entity.living.LivingEntityUseItemEvent;
@@ -16,11 +38,13 @@ import net.minecraftforge.event.level.BlockEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import org.apache.commons.compress.compressors.lz77support.LZ77Compressor;
+
+import java.util.Random;
 
 @Mod(DamMod.MOD_ID)
-public class ExampleMod extends DamMod implements IBlockBreakEvent, IServerStartEvent,
-        IItemPickupEvent, ILivingDamageEvent, IUseItemEvent, IFishedEvent,
-        IInteractEvent, IMovementEvent {
+public class ExampleMod extends DamMod implements ILivingDamageEvent {
 
     public ExampleMod(){
         super();
@@ -28,27 +52,7 @@ public class ExampleMod extends DamMod implements IBlockBreakEvent, IServerStart
 
     @Override
     public String autor() {
-        return "Javier Jorge Soteras";
-    }
-
-    @Override
-    @SubscribeEvent
-    public void onBlockBreak(BlockEvent.BreakEvent event) {
-        BlockPos pos = event.getPos();
-        System.out.println("Bloque destruido en la posicion "+pos);
-    }
-
-    @Override
-    @SubscribeEvent
-    public void onServerStart(ServerStartingEvent event) {
-        LOGGER.info("Server starting");
-    }
-
-    @Override
-    @SubscribeEvent
-    public void onItemPickup(EntityItemPickupEvent event) {
-        LOGGER.info("Item recogido");
-        System.out.println("Item recogido");
+        return "Jagoba Inda Arizaleta";
     }
 
     @Override
@@ -60,55 +64,107 @@ public class ExampleMod extends DamMod implements IBlockBreakEvent, IServerStart
     @Override
     @SubscribeEvent
     public void onLivingDeath(LivingDeathEvent event) {
-        System.out.println("evento LivingDeathEvent invocado "+event.getEntity().getClass()+" provocado por "+event.getSource().getEntity());
-
-    }
-
-    @Override
-    @SubscribeEvent
-    public void onUseItem(LivingEntityUseItemEvent event) {
-        LOGGER.info("evento LivingEntityUseItemEvent invocado "+event.getEntity().getClass());
-    }
-
-
-    @Override
-    @SubscribeEvent
-    public void onPlayerFish(ItemFishedEvent event) {
-        System.out.println("¡Has pescado un pez!");
-    }
-
-    @Override
-    @SubscribeEvent
-    public void onPlayerTouch(PlayerInteractEvent.RightClickBlock event) {
-        System.out.println("¡Has hecho click derecho!");
-        BlockPos pos = event.getPos();
-        BlockState state = event.getLevel().getBlockState(pos);
-        Player player = event.getEntity();
-        ItemStack heldItem = player.getMainHandItem();
-        if (ItemStack.EMPTY.equals(heldItem)) {
-            System.out.println("La mano esta vacia");
-            if (state.getBlock().getName().getString().trim().toLowerCase().endsWith("log")) {
-                System.out.println("¡Has hecho click sobre un tronco!");
-            }
+        //
+        Level world = event.getEntity().getLevel();
+        long momentoDelTia = world.getDayTime();
+        Vec3 posicionMuerte = event.getEntity().position();
+        BlockPos blockpos = new BlockPos(posicionMuerte.x, posicionMuerte.y, posicionMuerte.z);
+        BlockState state = Blocks.CHEST.defaultBlockState();
+        LivingEntity mobMuerto = event.getEntity();
+        if ((mobMuerto instanceof Skeleton) && !(momentoDelTia < 12000)) {
+            world.setBlock(blockpos, state,1);
         }
+
+        System.out.println("*".repeat(50));
+        System.out.println("*".repeat(50));
+        System.out.println("*".repeat(50));
+        System.out.println("*".repeat(50));
+        System.out.println(world);
+        System.out.println(momentoDelTia);
+        System.out.println(posicionMuerte);
+        System.out.println(mobMuerto);
+        System.out.println("*".repeat(50));
+        System.out.println("*".repeat(50));
+        System.out.println("*".repeat(50));
+        System.out.println("*".repeat(50));
     }
 
-    @Override
-    @SubscribeEvent
-    public void onPlayerWalk(MovementInputUpdateEvent event) {
-        if(event.getEntity() instanceof Player){
-            if(event.getInput().down){
-                System.out.println("down"+event.getInput().down);
+
+
+    public Item calcularItem() {
+        Item item = null;
+        item = Items.IRON_INGOT;
+        item = Items.GOLD_INGOT;
+        item = Items.BREAD;
+        item = Items.DIAMOND;
+        item = Items.EMERALD;
+        item = Items.REDSTONE;
+        item = Items.LAPIS_LAZULI;
+        item = Items.IRON_SWORD;
+        item = Items.GOLDEN_APPLE;
+        item = Items.EXPERIENCE_BOTTLE;
+        item = Items.COOKED_BEEF;
+        item = Items.PUMPKIN_PIE;
+        item = Items.ARROW;
+        item = Items.FISHING_ROD;
+        item = Items.BOW;
+        item = Items.SADDLE;
+        item = Items.DRAGON_BREATH;
+        item = Items.TOTEM_OF_UNDYING;
+        item = Items.NETHER_STAR;
+        item = Items.COAL;
+        item = Items.IRON_ORE;
+        item = Items.GOLD_ORE;
+        item = Items.DIAMOND_ORE;
+        item = Items.EMERALD_ORE;
+        item = Items.REDSTONE_ORE;
+        item = Items.LAPIS_BLOCK;
+        item = Items.IRON_BLOCK;
+        item = Items.GOLD_BLOCK;
+        item = Items.DIAMOND_BLOCK;
+        item = Items.EMERALD_BLOCK;
+        item = Items.BEACON;
+        item = Items.ENCHANTED_GOLDEN_APPLE;
+        item = Items.SHIELD;
+        item = Items.TRIDENT;
+        item = Items.TURTLE_HELMET;
+        item = Items.HEART_OF_THE_SEA;
+        item = Items.NAUTILUS_SHELL;
+        item = Items.END_CRYSTAL;
+        item = Items.ELYTRA;
+        item = Items.DRAGON_EGG;
+        item = Items.COMMAND_BLOCK;
+        item = Items.CAKE;
+
+
+
+        Random random = new Random();
+        int roll = random.nextInt(100000) + 1;
+        if (roll <= 10000) {
+            if (random.nextBoolean()) {
+                item = Items.IRON_INGOT;
+            } else {
+                item = Items.GOLD_INGOT;
             }
-            if(event.getInput().up){
-                System.out.println("up"+event.getInput().up);
-            }
-            if(event.getInput().right){
-                System.out.println("right"+event.getInput().right);
-            }
-            if(event.getInput().left){
-                System.out.println("left"+event.getInput().left);
-            }
+        } else if (roll <= 20000) {
+            item = Items.BREAD;
+        } else if (roll <= 30000) {
+            int semillaRoll = random.nextInt(8) + 1;
+            item = switch (semillaRoll) {
+                case 1 -> Items.WHEAT_SEEDS;
+                case 2 -> Items.PUMPKIN_SEEDS;
+                case 3 -> Items.BEETROOT_SEEDS;
+                case 4 -> Items.MELON_SEEDS;
+                case 5 -> Items.COCOA_BEANS;
+                case 6 -> Items.CARROT;
+                case 7 -> Items.POTATO;
+                case 8 -> Items.SWEET_BERRIES;
+                default -> item;
+            };
+        } else {
+            item = Items.DIAMOND;
         }
+        return item;
+
     }
 }
